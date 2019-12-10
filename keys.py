f2d3a324c4fcb656fa3a7b0956b49d504e876a51
Silvio Ankermann <silvio@booq.org>
2019-12-10 03:38:13 +0100

    Migrate all but export functions to

--- keys.py
+++ keys.py
@@ -4,17 +4,65 @@ from drive import DriveFolder
 from drive import NotAuthenticatedError
 
 class RemoteRoot():
-    def __init__(self, gauth_json, creds_json, rootpath):
-        self.drive = GoogleDrive(gauth_json, creds_json, autoconnect=True)
-        self.root = self.drive.create_path(rootpath)
+    def __init__(self, drive: GoogleDrive, root: DriveFolder):
+        self.drive = drive
+        self.root = root
+
+    @classmethod
+    def from_path(cls, gauth_json, creds_json, rootpath):
+        drive = GoogleDrive(gauth_json, creds_json)
+        drive.connect()
+        root = drive.create_path(rootpath)
+        return cls(drive, root)
+
+    @classmethod
+    def from_id(cls, gauth_json, creds_json, root_id):
+        drive = GoogleDrive(gauth_json, creds_json)
+        drive.connect()
+        root = drive.item_by_id(root_id)
+        if root.isfolder():
+            return cls(drive, root)
+        else:
+            raise FileNotFoundError("ID {} is not a folder".format(root_id))
+
+    @property
+    def id(self):
+        return self.root.id
 
     def json_creds(self):
         return self.drive.json_creds()
 
+    def get_key(self, key):
+        return Key(key, self.root.child(key, folders=False))
+
+    def new_key(self, key):
+        try:
+            return self.get_key(key)
+        except FileNotFoundError:
+            return Key(key, self.root.new_file(key))
+
+    def delete_key(self, key):
+        try:
+            remote_file = self.root.child(key)
+            remote_file.remove()
+        except FileNotFoundError:
+            pass
+
+    
+
 class Key():
-    def __init__(self, key, remote_file):
+    def __init__(self, key: str, remote_file: DriveFile):
         self.key = key
         self.file = remote_file
+        self.resumable_uri = None
+
+    def upload(self, local_filename: str, chunksize: int = None, progress_handler: callable = None):
+        self.file.upload(local_filename, chunksize=chunksize, resumable_uri=self.resumable_uri, progress_handler=progress_handler)
+
+    def download(self, local_filename: str, chunksize: int = None, progress_handler: callable = None):
+        self.file.download(local_filename, chunksize=chunksize, progress_handler=progress_handler)
+
+
 
 class ExportKey():
     def __init__(self, key, remote_file, path):
