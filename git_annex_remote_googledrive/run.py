399961ffab340a129f1e4ee0282944f4e369e93d
Silvio Ankermann <silvio@booq.org>
2020-02-01 19:55:31 +0100

    Add license info to source files

--- git_annex_remote_googledrive/run.py
+++ git_annex_remote_googledrive/run.py
@@ -1,3 +1,12 @@
+# Copyright (C) 2017-2020  Silvio Ankermann
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of version 3 of the GNU
+# General Public License as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+
 import sys, os
 
 from annexremote import Master
