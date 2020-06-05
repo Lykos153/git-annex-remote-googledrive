Lykos153/git-annex-remote-googledrive

commit 6b3a7097debfbb8ff524b89fcc57368a371057c9

Only show lockdown warning when using the built-in API key

git_annex_remote_googledrive/run.py
@@ -20,6 +20,8 @@ from drivelib import __version__ as drivelib_version
 from drivelib import GoogleDrive
 from .google_remote import GoogleRemote
 from . import __version__
+from . import _default_client_id as client_id
+from . import _default_client_secret as client_secret
 
 class bcolors:
     HEADER = '\033[95m'
@@ -69,8 +71,6 @@ def setup():
         print("Until this is settled you'll see a warning about this application not being verified by Google which you need to accept in order to proceed.")
         print("Read more on https://github.com/Lykos153/git-annex-remote-googledrive#google-drive-api-lockdown")
         print("======")
-        client_id = '275666578511-ndjt6mkns3vgb60cbo7csrjn6mbh8gbf.apps.googleusercontent.com'
-        client_secret = 'Den2tu08pRU4s5KeCp5whas_'
 
 
     gauth = {

git_annex_remote_googledrive/__init__.py
@@ -10,3 +10,5 @@
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+_default_client_id = '275666578511-ndjt6mkns3vgb60cbo7csrjn6mbh8gbf.apps.googleusercontent.com'
+_default_client_secret = 'Den2tu08pRU4s5KeCp5whas_'

git_annex_remote_googledrive/google_remote.py
@@ -15,7 +15,7 @@ from pathlib import Path
 from . import __version__
 from annexremote import __version__ as annexremote_version
 from drivelib import __version__ as drivelib_version
-
+from . import _default_client_id as DEFAULT_CLIENT_ID
 
 
 from drivelib import GoogleDrive
@@ -249,7 +249,9 @@ class GoogleRemote(annexremote.ExportRemote):
     def prepare(self):
         self._send_version()
 
-        if self.annex.getconfig('mute-api-lockdown-warning') != "true":
+        if self.annex.getconfig('mute-api-lockdown-warning') != "true" and \
+                json.loads(self.credentials)['client_id'] == DEFAULT_CLIENT_ID:
+
             self._info("====== git-annex-remote-googledrive")
             self._info("IMPORTANT: Google has started to lockdown their Google Drive API. This might affect access to your Google Drive remotes.")
             self._info("Please consider untrusting this remote until it is clear what happends next.")

commit 9960d86b6251d1373c498ffd5461076104f2867e

Fix bug in setup when using build-in API
Bug was introduced in 6b3a7097debfbb8ff524b89fcc57368a371057c9

git_annex_remote_googledrive/run.py
@@ -20,8 +20,8 @@ from drivelib import __version__ as drivelib_version
 from drivelib import GoogleDrive
 from .google_remote import GoogleRemote
 from . import __version__
-from . import _default_client_id as client_id
-from . import _default_client_secret as client_secret
+from . import _default_client_id as DEFAULT_CLIENT_ID
+from . import _default_client_secret as DEFAULT_CLIENT_SECRET
 
 class bcolors:
     HEADER = '\033[95m'
@@ -71,6 +71,8 @@ def setup():
         print("Until this is settled you'll see a warning about this application not being verified by Google which you need to accept in order to proceed.")
         print("Read more on https://github.com/Lykos153/git-annex-remote-googledrive#google-drive-api-lockdown")
         print("======")
+        client_id = DEFAULT_CLIENT_ID
+        client_secret = DEFAULT_CLIENT_SECRET
 
 
     gauth = {