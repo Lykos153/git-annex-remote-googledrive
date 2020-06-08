bc2fae0548a96da667fea3e9a1b52fbe9530b1f4
Silvio Ankermann <silvio@booq.org>
2020-06-08 10:17:15 +0200

    Unpin drivelib version

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     },
     install_requires=[
           'annexremote',
-          'drivelib==0.1.4',
+          'drivelib>=0.1.4',
           'gitpython',
           'tenacity',
           'humanfriendly',
