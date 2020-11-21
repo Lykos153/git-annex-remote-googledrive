Lykos153/git-annex-remote-googledrive

commit c08747d1a1c6026779fedbe4cd2f554d2395f16a

Repeat transfer when resumable_uri has expired or is invalid

git_annex_remote_googledrive/keys.py
@@ -114,8 +114,14 @@ class Key():
                              resumable_uri=self.resumable_uri,
                              progress_handler=self._upload_progress(progress_handler)
                             )
-        except CheckSumError:
-            logging.warning("Checksum mismatch. Repeating upload")
+        except (CheckSumError, HttpError) as e:
+            if isinstance(e, CheckSumError):
+                logging.warning("Checksum mismatch. Repeating upload")
+            elif isinstance(e, HttpError) and e.resp['status'] == '404':
+                logging.warning("Invalid resumable_uri. Probably expired. Repeating upload.")
+            else:
+                raise
+
             self.resumable_uri = None
             self.file.upload(local_filename,
                              chunksize=chunksize,

commit 3ac62c4f9b9676041c80ceda4e4b9f3c24b7397a

Fix retry of failed uploads
Introduced by c08747d1a1c6026779fedbe4cd2f554d2395f16a

git_annex_remote_googledrive/keys.py
@@ -129,15 +129,16 @@ class Key():
                 logging.warning("Invalid resumable_uri. Probably expired. Repeating upload.")
             else:
                 raise
-        except FileExistsError:
-            # Uploading an existing key is not an error
-            return
-
+            
             self.resumable_uri = None
             self.file.upload(local_filename,
                              chunksize=chunksize,
                              progress_handler=self._upload_progress(progress_handler)
                             )
+        except FileExistsError:
+            # Uploading an existing key is not an error
+            return
+
         self.resumable_uri = None
 
     @property