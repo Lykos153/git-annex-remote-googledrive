a1a65bb9b85437ac0b5491f0d43c945f7015ddae
Silvio Ankermann <silvio@booq.org>
2022-05-10 18:14:21 +0200

    Upgrade Versioneer to 0.22

--- git_annex_remote_googledrive/__init__.py
+++ git_annex_remote_googledrive/__init__.py
@@ -7,8 +7,7 @@
 # warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 #
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
 _default_client_id = '275666578511-ndjt6mkns3vgb60cbo7csrjn6mbh8gbf.apps.googleusercontent.com'
 _default_client_secret = 'Den2tu08pRU4s5KeCp5whas_'
